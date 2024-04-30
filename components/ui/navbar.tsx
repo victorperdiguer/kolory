69e9269f15c032323a0958e3ab0ca0064f31e3d2
Victor <victor.perdiguer@gmail.com>
2024-05-01 00:32:22 +0200

    touches on the logo positioning and padding

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -5,31 +5,33 @@ import Image from "next/image";
 import UserSessionComponent from "./authentication-ui/UserSessionComponent"
 
 const gluten = Gluten({
-    weight: "900",
+    weight: "500",
     subsets: ['latin']
 })
 
 
 function Navbar() {
 
-  return (
-    <nav className="p-4 border-b-2 lg:absolute w-full h-[4.1rem] bg-white z-10">
-      <div  className="flex justify-between text-white items-center">
-        <h1 className={gluten.className}>
-          KOLORY
-        </h1>
-        <div>
-          <Image
-            src="/kolorylogo.svg"
-            alt="logo"
-            width={50}
-            height={50}
-          />
+return (
+    <nav className="p-4 border-b-2 w-full h-[4.1rem] bg-white z-10">
+        <div className="flex justify-between text-white items-center">
+          <div className="flex justify-center items-center">
+            <div>
+                <Image
+                    src="/kolorylogo.svg"
+                    alt="logo"
+                    width={50}
+                    height={50}
+                />
+            </div>
+            <h1 className={gluten.className + " " + "relative top-1 text-4xl text-slate-900"}>
+                KOLORY
+            </h1>
+          </div>
+            <UserSessionComponent/>
         </div>
-        <UserSessionComponent/>
-      </div>
     </nav>
-  )
+)
 }
 
 export default Navbar
