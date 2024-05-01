c9d5fb532b64680e3a1b2d22691a61c8b17fb976
Victor <victor.perdiguer@gmail.com>
2024-05-01 21:22:34 +0200

    verification unique google user added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import axios from "axios"
+import connectMongoDB from "@/lib/mongodb"
+import User from "@/models/User"
 
 const handler = NextAuth({
   // Configure one or more authentication providers
@@ -8,7 +11,31 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({user, account, profile}): Promise<string | boolean> {
+        if (account?.provider === 'google'){
+          const { name, email } = user
+          try {
+            await connectMongoDB()
+            const userExists = await User.findOne({ email })
+            if (!userExists) {
+              const url = 'http://localhost:3000/api/user'
+              const data = { name, email }
+              const res = await axios.post(url, data, {
+                headers: {
+                  "Content-Type": "application/json"
+                }
+              })
+              console.log(res.data)
+            }
+          } catch (error) {
+            console.error(error)
+          }
+        }
+        return true;
+    }
+}
 })
 
 export {handler as GET, handler as POST}
---
+++ models/User.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema, models } from "mongoose";
+
+
+const userSchema = new Schema ({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  avatar: {
+    type: String,
+    required: false
+  }
+},
+{
+  timestamps: true
+}
+)
+
+const User = models.User || mongoose.model('User', userSchema)
+
+export default User
