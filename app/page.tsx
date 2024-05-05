74505feccdba137fc5858c3af869660a02e1d0bc
Victor <victor.perdiguer@gmail.com>
2024-05-05 19:05:50 +0200

    fixed landing pic + added animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,18 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
 import LandingPalette from "@/components/ui/landingpalette";
+import { Gluten } from "next/font/google";
+
+const gluten = Gluten({
+  weight: "700",
+  subsets: ['latin']
+})
 
 export default function Home() {
   return (
     <main className="flex flex-row items-center justify-around p-24">
       <div>
-        <h1 className="lg:text-7xl text-4xl max-w-[800px] tracking-tight mx-auto font-black text-center mb-[30px]">Connect with color</h1>
+        <h1 className="lg:text-7xl text-4xl max-w-[800px] tracking-tight mx-auto font-black text-center mb-[30px]"><span className={gluten.className}>Konnect</span> <span className="font-medium lg:text-4xl text-xl">with</span> <span className={gluten.className}>kolor</span></h1>
         <h2 className="text-lg max-w-[500px] mb-[35px] mx-auto font-medium text-[#464853] text-center">Perfect palettes are just a tap away.</h2>
         <h2 className="text-lg max-w-[500px] mb-[35px] mx-auto font-medium text-[#464853] text-center">Your hub for all things palette, from generation to application.</h2>
         <div className="flex justify-center">
@@ -18,12 +24,6 @@ export default function Home() {
       </div>
       <div>
         <LandingPalette />
-            {/* <Image
-                src="/pantonepalette.svg"
-                alt="palette"
-                width={350}
-                height={350}
-            /> */}
         </div>
     </main>
   );
