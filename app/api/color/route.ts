e1536fc8ef68b447e24d10369a950f1aefc03b66
Victor <victor.perdiguer@gmail.com>
2024-05-15 16:56:05 +0200

    added delete color feature

--- app/api/color/route.ts
+++ app/api/color/route.ts
@@ -2,6 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 import connectMongoDB from "@/lib/mongodb";
 import User from "@/models/User";
 
+export async function GET(req: NextRequest) {
+  const email = req.nextUrl.searchParams.get("email");
+  if (!email) {
+    return NextResponse.json({ message: "Email query parameter is required" }, { status: 400 });
+  }
+
+  await connectMongoDB();
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return NextResponse.json({ message: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({
+    savedColors: user.savedColors,
+  }, { status: 200 });
+}
+
 export async function POST(req: NextRequest) {
   const { email, color } = await req.json();
   await connectMongoDB();
@@ -13,15 +31,22 @@ export async function POST(req: NextRequest) {
   }
   console.log(user)
 
-  // Add color to the savedColors array if it's not already there
+  // Add color to the savedColors array if it's not already there. If it's there, it's a delete.
   if (!user.savedColors.includes(color)) {
     user.savedColors.push(color);
     await user.save();
+    return NextResponse.json({
+      message: "Color saved successfully",
+      color: color,
+      email: email
+    }, { status: 201 });
+  } else {
+    user.savedColors = user.savedColors.filter(savedColor => savedColor !== color);
+    await user.save();
+    return NextResponse.json({
+      message: "Color deleted successfully",
+      color: color,
+      email: email
+    }, { status: 201 });
   }
-
-  return NextResponse.json({
-    message: "Color saved successfully",
-    color: color,
-    email: email
-  }, { status: 201 });
 }
