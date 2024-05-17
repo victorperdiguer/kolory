4a7ffed4739db9fa4cf935e59b0ad45e981adf06
Victor <victor.perdiguer@gmail.com>
2024-05-17 15:15:45 +0200

    adding copy to clipboard feature

--- components/ui/subnavbar-menus/sharepalette.tsx
+++ components/ui/subnavbar-menus/sharepalette.tsx
@@ -3,18 +3,40 @@ import { Share2, Link, Image, File} from "lucide-react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "../dialog";
 import { Button } from "../button";
+import { Toast } from "../toast";
+import { useToast } from "../use-toast";
+import useCopy from "@/app/hooks/use-copy";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
 
 
 const SharePalette = ({params}: {params: {pattern: string}}) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const { copy } = useCopy();
+  const { toast } = useToast();
+  const pathname = usePathname();
+
+  const handleCopyUrl = () => {
+    // TODO - add live url
+    copy(pathname);
+    toast({
+      title: "URL copied to clipboard",
+    });
+
+    setOpen(false);
+  };
+
+
+
   return (
     <div>
-      <Dialog>
+      <Dialog open={open} onOpenChange={() => setOpen(true)}>
         <DialogTrigger asChild>
           <Button 
           variant="noborder"
@@ -27,7 +49,8 @@ const SharePalette = ({params}: {params: {pattern: string}}) => {
           <DialogHeader>
             <DialogTitle>Share palette</DialogTitle>
             <div className="flex justify-between items-center pt-6">
-              <Button variant="noborder">
+              <Button variant="noborder" onClick={handleCopyUrl}
+                >
                   <div className="pr-2"><Link /></div>
                   <h1>Link</h1>
               </Button>
