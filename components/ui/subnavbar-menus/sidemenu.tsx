280ae524437672ca1f1ccb66f12acf59ebb66910
Victor <victor.perdiguer@gmail.com>
2024-05-21 17:24:13 +0200

    WIP adding scrollability to saved palettes

--- components/ui/subnavbar-menus/sidemenu.tsx
+++ components/ui/subnavbar-menus/sidemenu.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react"
 import { Button } from "../button";
 import { useState } from "react";
-import { SwatchBook, Droplet } from "lucide-react";
+import { SwatchBook, Droplet, CircleOff } from "lucide-react";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import { Gluten } from "next/font/google";
+import { useRouter } from "next/navigation";
 
 const gluten = Gluten({
-  weight: "500",
+  weight: "300",
   subsets: ['latin']
 })
 
@@ -17,6 +18,7 @@ const SideMenu = () => {
   const { data: session, status } = useSession();
   const [savedPalettes, setSavedPalettes] = useState([]);
   const [savedColors, setSavedColors] = useState([]);
+  const navigate = useRouter();
 
   useEffect(() => {
     const fetchSavedColors = async () => {
@@ -47,6 +49,10 @@ const SideMenu = () => {
 
     fetchSavedPalettes();
   }, [status, session, itemDisplay]);
+
+  const accessPalette = (palette: string[]) => {
+    navigate.push(`/colors/${palette.join('-')}`);
+  }
     
   return (
     <div>
@@ -72,46 +78,49 @@ const SideMenu = () => {
           />
         </Button>
       </div>
-      <div>
+      <div className="p-3">
         {itemDisplay === "saved-palettes" && savedPalettes.length > 0 ? (
-          <div>
-            {savedPalettes.map((palette, index) => (
-              <div
-                key={index}
-                className="p-3 flex flex-row w-full justify-center m-auto"
-              >
-                {palette.map((color, index) => (
-                  <div
+          <div className="h-screen flex-1 overflow-auto">
+            {savedPalettes.map((palette: string[], index: number) => (
+                <div
                     key={index}
-                    style={{ backgroundColor: "#" + color }}
-                    className="flex-1 h-12" // flex-1 to take equal space, h-12 for height
-                  ></div>
-                ))}
-              </div>
+                    className="mb-3 flex flex-row w-full justify-center m-auto cursor-pointer"
+                    onClick={() => {accessPalette(palette)}}
+                >
+                    {palette.map((color: string, index: number) => (
+                        <div
+                            key={index}
+                            style={{ backgroundColor: "#" + color }}
+                            className="flex-1 h-12" // flex-1 to take equal space, h-12 for height
+                        ></div>
+                    ))}
+                </div>
             ))}
           </div>
         ) : null}
 
-        {itemDisplay === "saved-colors" && savedColors.length === 0 ? (
-          <div className="p-3">
-            <p className={gluten.className}>No saved palettes</p>
+        {itemDisplay === "saved-palettes" && savedPalettes.length === 0 ? (
+          <div className="p-3 flex flex-col justify-center gap-5 items-center">
+            <CircleOff color="#d1d1d1"/>
+            <p className={gluten.className + " " + "text-center"} style={{color: "#d1d1d1"}}>No saved palettes</p>
           </div>
         ) : null}
 
         {itemDisplay === "saved-colors" && savedColors.length > 0 ? (
           <div>
-            {savedColors.map((color, index) => (
+            {savedColors.map((color: string, index: number) => (
               <div key={index} className="p-3 flex flex-row justify-center items-center">
                 <Droplet fill={"#"+color} color={"#"+color}/>
-                <p className={gluten.className}>{color.toUpperCase()}</p>
+                <p className={gluten.className+" "+"mt-1"}>{color.toUpperCase()}</p>
               </div>
             ))}
           </div>
         ) : null}
 
         {itemDisplay === "saved-colors" && savedColors.length === 0 ? (
-          <div className="p-3">
-            <p className={gluten.className}>No saved colors</p>
+          <div className="p-3 flex flex-col justify-center gap-5 items-center">
+            <CircleOff color="#d1d1d1"/>
+            <p className={gluten.className + " " + "text-center"} style={{color: "#d1d1d1"}}>No saved colors</p>
           </div>
         ) : null}
       </div>
